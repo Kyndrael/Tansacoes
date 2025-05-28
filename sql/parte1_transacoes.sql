adfb1f952248a268897450c50e95e58262a0fbe5
Kyndrael <jonathansilveira95@hotmail.com>
2025-05-28 10:15:36 -0300

    Update parte1_transacoes.sql

--- sql/parte1_transacoes.sql
+++ sql/parte1_transacoes.sql
@@ -1,10 +1,15 @@
--- Desabilita o autocommit
+-- Desabilita autocommit
 SET autocommit = 0;
 
+-- Inicia transação
 START TRANSACTION;
 
--- Exemplo de movimentação bancária
-UPDATE contas SET saldo = saldo - 500 WHERE id = 1;
-UPDATE contas SET saldo = saldo + 500 WHERE id = 2;
+-- Transferência de R$100 da conta 1 para a conta 2
+UPDATE contas SET saldo = saldo - 100 WHERE id = 1;
+UPDATE contas SET saldo = saldo + 100 WHERE id = 2;
 
+-- Finaliza transação
 COMMIT;
+
+-- Para desfazer, usar:
+-- ROLLBACK;
